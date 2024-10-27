228e0c1f51e9d68d6ba391ba0c5fd0eaaa578b5c
K.M.I.I.Madhushani <139638076+ImeshaMadhushani@users.noreply.github.com>
2024-10-27 19:23:13 +0530

    complete home page without visit button functionality

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
     return(
     <>
         <header className='flex items-center justify-center py-4 mt-20'>
-            <h1 className='text-6xl font-bold text-[#0B192C] '>SunShine Villa</h1>
+            <h1 className='text-6xl font-bold text-[#0B192C] stroke-text'>SunShine Villa</h1>
         </header>
         </>
     )
--- src/pages/client/home/homepage.jsx
+++ src/pages/client/home/homepage.jsx
@@ -1,15 +1,20 @@
 import Header from "../../../components/header/Header";
 import "./homepage.css"
 export default function HomePage() {
+
+    function visit() {
+        alert('Visiting SunShine Villa is currently unavailable, please visit us at our other locations.')
+    }
+
     return (
         <>
           
             <div className="relative flex flex-col items-center justify-center h-screen text-center bg-cover bg-center p-12 overlay">
                 <Header />
                 <div className='mt-10 relative z-10'>
-                <h2 className="text-4xl font-semibold text-[white]">Welcome to SunShine Villa</h2>
-                <p className="text-lg text-gray-200 mt-4">Discover our beautiful and luxurious hotel</p>
-                <button className="mt-6 px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-800">Visit Us</button>
+                <h2 className="text-4xl font-semibold text-[white] stroke-text1">Welcome to SunShine Villa</h2>
+                <p className="text-lg text-gray-200 mt-4 stroke-text2">Discover our beautiful and luxurious hotel</p>
+                <button className="mt-6 px-6 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-800" onClick={visit}>Visit Us</button>
                 </div>
             </div>
         </>
