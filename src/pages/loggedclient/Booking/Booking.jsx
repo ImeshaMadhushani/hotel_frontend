15d89b3f50bbd0dababd3651b692a4e99dea0d43
K.M.I.I.Madhushani <139638076+ImeshaMadhushani@users.noreply.github.com>
2024-10-30 18:51:39 +0530

    outline booking page

--- src/pages/loggedclient/Booking/Booking.jsx
+++ src/pages/loggedclient/Booking/Booking.jsx
@@ -6,13 +6,34 @@ export default function Booking() {
      const user = JSON.parse(localStorage.getItem("user"));
 
     return(
-    <div className="bgbooking">
+    <div className="">
             <div>
              <NavBarLogged user={user} />
             </div>
 
             <div>
-                
+                <div className="bg-white border border-gray-200 p-6 rounded-lg shadow-lg flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4">
+            <input 
+              type="date" 
+              className="border border-gray-300 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+            <input 
+              type="date" 
+              className="border border-gray-300 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+            <select 
+              className="border border-gray-300 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            >
+              <option>Luxury</option>
+              <option>Deluxe</option>
+              <option>Standard</option>
+            </select>
+            <button 
+              className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-300"
+            >
+              Book Now
+            </button>
+          </div>
             </div>
             
         </div>
